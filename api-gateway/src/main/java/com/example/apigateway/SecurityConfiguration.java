809fdfbfec73f38be0179a946c35abb9baacd54e
anvesh <ks.anvesh@gmail.com>
2021-10-12 07:15:22 -0500

    add /greeting to api-gateway

---
+++ api-gateway/src/main/java/com/example/apigateway/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.savedrequest.NoOpServerRequestCache;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        // @formatter:off
+        http
+//                .requestCache()
+//                .requestCache(NoOpServerRequestCache.getInstance())
+//                .and()
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+            .oauth2Login()
+                .and()
+            .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+        // @formatter:on
+    }
+}
